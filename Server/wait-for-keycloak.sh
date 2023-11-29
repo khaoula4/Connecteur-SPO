06cab4c6ff0afc585fcd40c7cdbd3e09a2fd301e
khaoula <khaoulajbari1@gmail.com>
2023-11-29 13:55:40 +0100

    Adding-Keycloak-Aut

---
+++ Server/wait-for-keycloak.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Wait for Keycloak to be healthy
+echo "Waiting for Keycloak to be healthy..."
+until $(curl --output /dev/null --silent --head --fail http://keycloak:8080/auth); do
+    printf '.'
+    sleep 5
+done
+
+echo "Keycloak is up and running."
+
+# Start the main application
+exec "$@"
